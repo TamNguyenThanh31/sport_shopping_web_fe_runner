821f6e3bf0ba048954321de6e5b2d2237a227a59
DESKTOP-FGS8S49\Admin <tamnguyencv31@gmail.com>
2025-04-14 18:23:41 +0700

    fix: them themes và icon vao app.modules

--- running-shop/src/app/app.module.ts
+++ running-shop/src/app/app.module.ts
@@ -1,13 +1,13 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
-import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SharedModule } from './shared/shared.module';
-import { TooltipModule } from 'primeng/tooltip';
-import { PanelMenuModule } from 'primeng/panelmenu';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {FormsModule} from '@angular/forms';
+import {HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {SharedModule} from './shared/shared.module';
+import {TooltipModule} from 'primeng/tooltip';
+import {PanelMenuModule} from 'primeng/panelmenu';
 import {MessageService} from "primeng/api";
 import {AuthInterceptor} from "./core/interceptors/auth.interceptor";
 import {ToastModule} from "primeng/toast";
@@ -19,27 +19,47 @@ import {InputTextModule} from "primeng/inputtext";
 import {DropdownModule} from "primeng/dropdown";
 import {CheckboxModule} from "primeng/checkbox";
 
-@NgModule({ declarations: [AppComponent],
-    bootstrap: [AppComponent], imports: [BrowserModule,
-        AppRoutingModule,
-        FormsModule,
-        BrowserAnimationsModule,
-        TooltipModule,
-        SharedModule,
-        PanelMenuModule,
-        // PrimeNG
-        PanelMenuModule,
-        DataViewModule,
-        CardModule,
-        ButtonModule,
-        ListboxModule,
-        InputTextModule,
-        DropdownModule,
-        CheckboxModule,
-        ToastModule], providers: [MessageService,
-        {
-            provide: HTTP_INTERCEPTORS,
-            useClass: AuthInterceptor,
-            multi: true,
-        }, provideHttpClient(withInterceptorsFromDi()),] })
-export class AppModule {}
+import {ApplicationConfig} from '@angular/core';
+import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
+import {providePrimeNG} from 'primeng/config';
+import Aura from '@primeng/themes/lara';
+import "primeicons/primeicons.css";
+
+
+@NgModule({
+  declarations: [AppComponent],
+  bootstrap: [AppComponent], imports: [BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    TooltipModule,
+    SharedModule,
+    PanelMenuModule,
+    // PrimeNG
+    PanelMenuModule,
+    DataViewModule,
+    CardModule,
+    ButtonModule,
+    ListboxModule,
+    InputTextModule,
+    DropdownModule,
+    CheckboxModule,
+    ToastModule],
+  providers: [
+    MessageService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true,
+    },
+    provideHttpClient(withInterceptorsFromDi()),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Aura
+      }
+    })
+  ]
+})
+export class AppModule {
+}
