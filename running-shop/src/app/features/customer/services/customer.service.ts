ea6286f24ba906f5a731609c2eb1bdc1027ef49b
DESKTOP-FGS8S49\Admin <tamnguyencv31@gmail.com>
2025-07-10 14:23:17 +0700

    update: doi mat khau co check mat khau cu

--- running-shop/src/app/features/customer/services/customer.service.ts
+++ running-shop/src/app/features/customer/services/customer.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import {Observable} from "rxjs";
 import {UserDTO} from "../../../shared/models/userDTO.model";
+import {UpdatePassword} from "../../../shared/models/update-password.model";
 
 @Injectable({
   providedIn: 'root'
@@ -14,5 +15,12 @@ export class CustomerService {
 
   updateProfile(user: UserDTO): Observable<UserDTO> {
     return this.http.put<UserDTO>(`${this.apiUrl}/update-customer`, user);
+  }
+
+  changePassword(updatePassword: UpdatePassword): Observable<any> {
+    return this.http.put(`${this.apiUrl}/change-password`, updatePassword, { 
+      observe: 'response',
+      responseType: 'text'
+    });
   }
 }
